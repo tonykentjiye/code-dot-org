5256ce5f94abec89ec08d0313d54a1e0630787b4
Josh Lory <josh.lory@code.org>
2016-09-20 15:45:52 -0700

    react-motion WIP

--- apps/src/code-studio/levelbuilder_edit_script.js
+++ apps/src/code-studio/levelbuilder_edit_script.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Motion, spring} from 'react-motion';
 
 /**
  * TODO
@@ -12,8 +13,35 @@ var TestComponent = React.createClass({
     // assetChosen: React.PropTypes.func.isRequired
   },
 
+  getInitialState() {
+    return {reverse: true, currentStyle: spring(800, {stiffness: 300, damping: 50})};
+  },
+
+  reset() {
+    this.setState({
+      reverse: !this.state.reverse,
+      currentStyle: spring(this.state.reverse ? 0 : 800, {stiffness: this.refs.stiffness.value, damping: this.refs.damping.value})
+    });
+  },
+
   render() {
-    return <div>Hello world</div>;
+    return (
+      <div>
+        <Motion defaultStyle={{x: 0}} style={{x: this.state.currentStyle}}>
+          {value =>
+            <div
+              style={{background: '#0f0', marginLeft: value.x, width: 100, height: 100}}
+            >
+              Hello world
+            </div>
+          }
+        </Motion>
+        <br/>
+        <p><input type="range" min="1" max="1000" defaultValue="250" ref="stiffness"/> Stiffness</p>
+        <p><input type="range" min="1" max="100" defaultValue="7" ref="damping"/> Damping</p>
+        <button onClick={this.reset}>Animate</button>
+      </div>
+    );
   }
 });
 
