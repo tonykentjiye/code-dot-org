60be8ef6f8bbb73acff7a4c720735519e4d7e6c3
Josh Lory <josh.lory@code.org>
2016-09-27 16:55:53 -0700

    WIP

--- apps/src/code-studio/levelbuilder_edit_script.js
+++ apps/src/code-studio/levelbuilder_edit_script.js
@@ -1,4 +1,5 @@
 /** @file JavaScript run only on the /s/:script_name/edit page. */
+/* global scriptData, i18nData, keyList */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -8,25 +9,39 @@ import VirtualizedSelect from 'react-virtualized-select';
 import 'react-virtualized/styles.css';
 import 'react-select/dist/react-select.css';
 import 'react-virtualized-select/styles.css';
+import color from '../color';
 
 const styles = {
   stage: {
     border: '1px solid #ccc',
     borderRadius: 5,
     margin: '10px 0',
-    padding: 20
+    padding: 20,
+    overflow: 'hidden'
   },
   stageName: {
     marginTop: 0
   },
   level: {
+    float: 'left',
+    clear: 'both',
+    padding: '3px 6px',
+    margin: '3px 0',
+    background: color.cyan,
+    color: '#fff',
+    borderRadius: '4px',
     ':hover': {
-      fontWeight: 'bold'
+      background: color.light_cyan
     }
+  },
+  remove: {
+    marginLeft: 10
   }
 };
 
-const ScriptEditor = Radium(React.createClass({
+const DRAG_THRESHOLD = 5;
+
+const ScriptEditor = React.createClass({
   propTypes: {
     scriptData: React.PropTypes.object.isRequired,
     i18nData: React.PropTypes.object.isRequired,
@@ -35,6 +50,37 @@ const ScriptEditor = Radium(React.createClass({
 
   getInitialState() {
     return {};
+  },
+
+  componentDidMount() {
+    window.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('mouseup', this.handleMouseUp);
+  },
+
+  handleMouseDown(index, {pageY}) {
+    this.setState({
+      dragging: index,
+      delta: 0,
+      overThreshold: false,
+      startingPageY: pageY
+    });
+  },
+
+  handleMouseMove({pageY}) {
+    if (this.state.dragging > -1) {
+      const delta = pageY - this.state.startingPageY;
+      this.setState({
+        delta,
+        overThreshold: this.state.overThreshold || Math.abs(delta) > DRAG_THRESHOLD
+      });
+    }
+  },
+
+  handleMouseUp() {
+    this.setState({
+      dragging: -1,
+      overThreshold: false
+    });
   },
 
   updatePlc() {
@@ -62,11 +108,49 @@ const ScriptEditor = Radium(React.createClass({
         <label>Allow stages to be hidden by teachers <input type="checkbox" /></label>
         <label>Wrap-up video <input defaultValue={this.props.scriptData.wrapup_video} /></label>
         <VirtualizedSelect options={this.props.keyList} clearable={false} />
-        <StageList stages={this.props.scriptData.stages} />
+        <div>
+          {this.props.scriptData.stages.map(stage =>
+            <div style={styles.stage} key={stage.title}>
+              <h2 style={styles.stageName}>{stage.title}</h2>
+              <div>
+                {stage.levels.map((level, levelIndex) => {
+                  const config = {stiffness: 500, damping: 30};
+                  const style = this.state.overThreshold && this.state.dragging === levelIndex ? {
+                      y: this.state.delta,
+                      scale: spring(1.1, config),
+                      shadow: spring(7, config)
+                    } : {
+                      y: 0,
+                      scale: 1,
+                      shadow: 0
+                    };
+                  return (
+                    <Motion style={style} key={levelIndex}>
+                      {({y, scale, shadow}) =>
+                        <div
+                          onMouseDown={this.handleMouseDown.bind(null, levelIndex)}
+                          style={Object.assign({}, styles.level, {
+                            transform: `translate3d(0, ${y}px, 0) scale(${scale})`,
+                            boxShadow: `${color.shadow} 0 ${shadow}px ${shadow * 2}px`,
+                            zIndex: this.state.dragging === levelIndex ? 1000 : 'auto'
+                          })}
+                          key={stage.title + ':' + level.name}
+                        >
+                          {level.name}
+                          <i className="fa fa-times" style={styles.remove}/>
+                        </div>
+                      }
+                    </Motion>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </div>
       </form>
     );
   }
-}));
+});
 
 const StageList = Radium(React.createClass({
   propTypes: {
@@ -79,7 +163,14 @@ const StageList = Radium(React.createClass({
         {this.props.stages.map(stage =>
           <div style={styles.stage} key={stage.title}>
             <h2 style={styles.stageName}>{stage.title}</h2>
-            <LevelList levels={stage.levels} />
+            <div>
+              {stage.levels.map(level =>
+                <div style={styles.level} key={level.title}>
+                  {level.name}
+                  <i className="fa fa-times" style={styles.remove} />
+                </div>
+              )}
+            </div>
           </div>
         )}
       </div>
@@ -98,6 +189,7 @@ const LevelList = Radium(React.createClass({
         {this.props.levels.map(level =>
           <div style={styles.level} key={level.title}>
             {level.name}
+            <i className="fa fa-times" style={styles.remove} />
           </div>
         )}
       </div>
